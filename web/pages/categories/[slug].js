d375015ad0fe38cd364bf2dd8ebea71d21783826
Jim Orluk <jim.orluk@gmail.comgit config --global user.email jim.orluk@gmail.comgit config --global user.email jim.orluk@gmail.com>
2021-04-15 16:46:37 +0000

    Fixed missing key on category page

--- web/pages/categories/[slug].js
+++ web/pages/categories/[slug].js
@@ -35,6 +35,7 @@ export async function getStaticProps(props) {
   "postList": *[_type == 'post' &&
   
     ^._id in categories[]._ref]{
+      _id,
    title,
    protectedPage,
    slug
@@ -48,106 +49,3 @@ export async function getStaticProps(props) {
 }
 
 export default Category
-
-// const MainContent = styled.div`
-//   width: 100%;
-//   display: grid;
-
-//   .intro {
-//     margin: auto;
-//     max-width: 800px;
-//     border-radius: 5px;
-//     padding: 0 0.5rem;
-//     ul {
-//       list-style: none;
-//       padding-left: 0.75rem;
-//       margin: 0;
-//     }
-//     li:not(:last-of-type) {
-//       padding-bottom: 0.25rem;
-//     }
-
-//     strong {
-//       /* color: var(--accent-dark); */
-//     }
-//   }
-// `
-// const Home = ({ posts, categoryList }) => {
-//   const [session] = useSession()
-//   return (
-//     <>
-//       <Head>
-//         <title>Berman Tech Tips</title>
-//       </Head>
-//       <Page>
-//         <MainContent>
-//           <h1> Berman Tech Tips</h1>
-//           <div className='intro '>
-//             <p className='accent_text'>Having trouble? We're here to help! </p>
-
-//             <p>
-//               Let's be real: things are not always going to go as planned. Even
-//               if it's all set up exactly right, sometimes technology just won't
-//               work. How could it when all these things can break it?
-//             </p>
-//             <ul>
-//               <li>
-//                 <strong>Mythical Beings:</strong> Dybbuk, gremlins
-//               </li>
-//               <li>
-//                 <strong>Astronomy: </strong> full moon, solar flares, Is Mercury
-//                 in retrograde?
-//               </li>
-//               <li>
-//                 <strong>Personal Troubles: </strong> computers don't like me,
-//                 waking up on the wrong side of the bed
-//               </li>
-//             </ul>
-//             <p>
-//               But there's good news! Technology can be made to work, and
-//               <strong> You Can Do It!</strong> Whether problems have already
-//               happened, or you want to <strong>be ready</strong> when they do,
-//               there's something here for you. You'll find a listing of all of
-//               our
-//               <strong> help topics below</strong>.
-//             </p>
-//             {session && (
-//               <>
-//                 <p>
-//                   Not finding the topic you want? Click that Suggest a Topic
-//                   link up there at the top!
-//                 </p>
-//                 <p>
-//                   Still having trouble solving your problem? Or not feeling like
-//                   trying? It's Ok! Click the Get Help link.
-//                 </p>
-//               </>
-//             )}
-//             {!session && (
-//               <p>
-//                 <strong>
-//                   Log in with your Berman account for full access to all the
-//                   help topics and to be able suggest topics and ask for help.
-//                 </strong>
-//               </p>
-//             )}
-//           </div>
-//           <CategoryCardHolder>
-//             {categoryList.map((category) => (
-//               <CategoryCard
-//                 posts={getFilteredPosts({
-//                   categoryId: category._id,
-//                   postList: posts,
-//                 })}
-//                 category={category}
-//                 key={category._id}
-//               />
-//             ))}
-//           </CategoryCardHolder>
-//         </MainContent>
-//       </Page>
-//     </>
-//   )
-// }
-
-// export default Home
