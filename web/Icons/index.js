2ce6128b79db02f331e8d78de8d35c8aad141139
Jim Orluk <jim.orluk@gmail.com>
2021-05-04 21:54:12 -0400

    Added NewUser icon

--- web/Icons/index.js
+++ web/Icons/index.js
@@ -1,27 +1,29 @@
-import Wifi from './Wifi.svg'
-import General from './General.svg'
-import Owl from './Owl.svg'
-import DockingStation from './DockingStation.svg'
-import SmartBoard from './SmartBoard.svg'
-import Zoom from './Zoom.svg'
 import BermanLogo from './BermanLogo.svg'
-import Splat from './Splat.svg'
-import Lock from './Lock.svg'
-import Youtube from './Youtube.svg'
+import DockingStation from './DockingStation.svg'
 import Facebook from './Facebook.svg'
+import General from './General.svg'
 import Instagram from './Instagram.svg'
+import Lock from './Lock.svg'
+import NewUser from './NewUser.svg'
+import Owl from './Owl.svg'
+import SmartBoard from './SmartBoard.svg'
+import Splat from './Splat.svg'
+import Wifi from './Wifi.svg'
+import Youtube from './Youtube.svg'
+import Zoom from './Zoom.svg'
 
 export {
-  Wifi,
-  General,
-  Owl,
-  DockingStation,
-  SmartBoard,
-  Zoom,
   BermanLogo,
-  Splat,
-  Lock,
-  Youtube,
-  Instagram,
+  DockingStation,
   Facebook,
+  General,
+  Instagram,
+  Lock,
+  NewUser,
+  Owl,
+  SmartBoard,
+  Splat,
+  Wifi,
+  Youtube,
+  Zoom,
 }
